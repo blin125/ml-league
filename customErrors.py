cc8ba3d48478f5581bf49f808c5e308ff57418f5
Bruno William <blin125@aucklanduni.ac.nz>
2024-01-04 15:27:31 +1300

    added scripts to download all the base img of league of legends up to patch 13.24

---
+++ customErrors.py
@@ -0,0 +1,5 @@
+class EmptyChampionList(Exception):
+    """ Custom exception for an empty champion list. """
+
+class FailFetch(Exception):
+    """ Custom exception for an empty champion list. """
